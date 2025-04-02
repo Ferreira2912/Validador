10884755972d4b113959c744b2bac2ef4b1ff6d5
Ferreira2912 <marquesferreirajoaopedro77@gmail.com>
2025-04-01 21:27:47 -0300

    add validação de email e senha utilizando regex

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ function testarSenha() {
     const resultado = validarSenha(senha) ? "✅ Senha válida" : "❌ Senha inválida (mín. 8 caracteres, 1 número, 1 maiúscula)";
     document.getElementById("resultadoSenha").textContent = resultado;
 }
+
+function validarEmail(email) {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+
+function validarSenha(senha) {
+    const regex = /^(?=.*[A-Z])(?=.*\d).{8,}$/;
+    return regex.test(senha);
+}
